77aa77f58b3ab1ea2039af453eb3dc15a7faafb3
lvliqi <lvliqi@360.net>
2017-11-25 11:10:25 +0800

    fix

---
+++ src/common/nav.js
@@ -0,0 +1,26 @@
+import dynamic from 'dva/dynamic';
+
+// wrapper of dynamic
+const dynamicWrapper = (app, models, component) => dynamic({
+  app,
+  models: () => models.map(m => import(`../models/${m}.js`)),
+  component,
+});
+
+// nav data
+export const getNavData = app => [
+  {
+    component: dynamicWrapper(app, ['user'], () => import('../layouts/BasicLayout')),
+    layout: 'BasicLayout',
+    name: '首页', // for breadcrumb
+    path: '/',
+    children: [
+      {
+        name: '查询表格',
+        path: 'table-list',
+        component: dynamicWrapper(app, ['rule'], () => import('../routes/List/TableList')),
+      },
+    ],
+  },
+];
+
